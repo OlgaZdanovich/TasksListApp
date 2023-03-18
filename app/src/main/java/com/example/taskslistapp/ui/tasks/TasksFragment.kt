e0b8127c53030464428b91c1fa2cae1c02ea357b
OlgaZdanovich <erochenko92@mail.ru>
2023-03-18 14:47:56 +0300

    ListAdapter with View Binding

--- app/src/main/java/com/example/taskslistapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/taskslistapp/ui/tasks/TasksFragment.kt
@@ -1,11 +1,35 @@
 package com.example.taskslistapp.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskslistapp.R
+import com.example.taskslistapp.databinding.FragmentTaskBinding
 import dagger.hilt.android.AndroidEntryPoint
 
+@Suppress("DEPRECATION")
 @AndroidEntryPoint
 class TasksFragment:Fragment(R.layout.fragment_task) {
     private val viewModel: TasksViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTaskBinding.bind(view)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 }
