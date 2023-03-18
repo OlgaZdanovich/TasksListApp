090ce71c8c128b2201f13e18059bc2ec20ac5f18
OlgaZdanovich <erochenko92@mail.ru>
2023-03-18 16:37:40 +0300

    Menu. Room search

--- app/src/main/java/com/example/taskslistapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/taskslistapp/ui/tasks/TasksFragment.kt
@@ -1,13 +1,18 @@
 package com.example.taskslistapp.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskslistapp.R
 import com.example.taskslistapp.databinding.FragmentTaskBinding
+import com.example.taskslistapp.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @Suppress("DEPRECATION")
@@ -30,6 +35,39 @@ class TasksFragment:Fragment(R.layout.fragment_task) {
         }
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
+        }
+        setHasOptionsMenu(true)
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name -> {
+
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
